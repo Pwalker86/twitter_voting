f85898e2c3c0d1374f21eb0d365eeef5c70f494b
Phil Walker <phil1586@gmail.com>
2015-12-12 20:22:45 -0700

    re-initialize

---
+++ walker_bot.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'dotenv'
+Dotenv.load
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.access_token = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+tweets = ['Gordon Hayward', 'Derrick Favors', 'Rudy Gobert']
+
+puts "#{ENV['CONSUMER_KEY']}"
+puts "done"
+
+
+client.update("#{tweets[rand(tweets.size)]} #NBABallot")
